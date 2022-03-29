c32f13085f51d133191b251141d45c35ecee13d5
janice83 <janna.lofman@gmail.com>
2022-03-29 13:49:31 +0300

    add front-page and list-page components

--- hkiapp/src/app/app.module.ts
+++ hkiapp/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FrontPageComponent } from './front-page/front-page.component';
+import { PlacesListComponent } from './places-list/places-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontPageComponent,
+    PlacesListComponent
   ],
   imports: [
     BrowserModule

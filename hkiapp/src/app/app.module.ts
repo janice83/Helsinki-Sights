a7a4572c08fb6608e42ef7ab4a09902d698bc62a
Josephine <josephine.closan@yahoo.com>
2022-03-31 13:29:16 +0300

    create back-button component and directive

--- hkiapp/src/app/app.module.ts
+++ hkiapp/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 import { FrontPageComponent } from './front-page/front-page.component';
 import { PlacesListComponent } from './places-list/places-list.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BackButtonComponent } from './back-button/back-button.component';
+import { BackButtonDirective } from './back-button.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     FrontPageComponent,
-    PlacesListComponent
+    PlacesListComponent,
+    BackButtonComponent,
+    BackButtonDirective
   ],
   imports: [
     BrowserModule,

9973122f1b337f013849c85215096f0ce6c43aa5
janice83 <janna.lofman@gmail.com>
2022-04-07 11:22:19 +0300

    remove comments from places-list

--- hkiapp/src/app/places-list/places-list.component.ts
+++ hkiapp/src/app/places-list/places-list.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PlacesService } from '../places.service';
 
-declare const L: any;
 @Component({
   selector: 'app-places-list',
   templateUrl: './places-list.component.html',
@@ -9,75 +8,11 @@ declare const L: any;
 })
 export class PlacesListComponent implements OnInit {
   places: any = []
-  lat!: number;
-  lon!: number;
 
   constructor(public placesService: PlacesService) { }
 
   ngOnInit(): void {
     this.getAllPlaces();
-
-    // if (!navigator.geolocation) {
-    //   console.log("location is not supported")
-    // }
-    // navigator.geolocation.getCurrentPosition((position) => {
-    //   const coords = position.coords;
-    //   const latLong = [coords.latitude, coords.longitude];
-
-      // Display latitude and longitude in console
-
-      // console.log(
-      //   `lat: ${position.coords.latitude}, lon: ${position.coords.longitude}`
-
-      // );
-      // Create a layer map 
-      // let mymap = L.map('map').setView(latLong, 13);
-
-  //     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibmFyaWs0MTAyIiwiYSI6ImNsMWtnazY5MTAwbXAzam9kMm52bWI5dm8ifQ.7PfvxSz3IrVKeeEyQ374rA', {
-  //       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-  //       maxZoom: 18,
-  //       id: 'mapbox/streets-v11',
-  //       tileSize: 512,
-  //       zoomOffset: -1,
-  //       accessToken: 'your.mapbox.access.token'
-  //     }).addTo(mymap);
-
-  //     let marker = L.marker(latLong).addTo(mymap);
-
-
-
-  //     // Add the marker on the chart
-  //     marker.bindPopup("Olet tässä").openPopup();
-
-
-  //     this.lat = position.coords.latitude;
-  //     this.lon = position.coords.longitude;
-
-  //   });
-  //  this.watchPosition;
-  // }
-
-  // watchPosition() {
-  //   let desLat = 0;
-  //   let desLon = 0;
-
-  //   let id = navigator.geolocation.watchPosition((position) => {
-  //     console.log(
-  //       `lat: ${position.coords.latitude}, lon: ${position.coords.longitude}`
-
-  //     );
-  //     if (position.coords.latitude === desLat) {
-  //       navigator.geolocation.clearWatch(id);
-  //     }
-  //   }, (err) => {
-  //     console.log(err);
-  //   }, {
-  //     enableHighAccuracy: true,
-  //     timeout: 5000,
-  //     maximumAge: 0
-  //   })
-
-
 
   }
 

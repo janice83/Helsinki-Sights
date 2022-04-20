f509ec484c2dd18e616478dfe9dc7052c7ed336f
janice83 <janna.lofman@gmail.com>
2022-04-20 13:20:23 +0300

    test sorting for dates in events-list

--- hkiapp/src/app/events/events.component.ts
+++ hkiapp/src/app/events/events.component.ts
@@ -21,6 +21,7 @@ export class EventsComponent implements OnInit {
   getAllEvents(): void {
     this.eventsService.getAllEvents().subscribe((res: any) => {
       this.events = res
+      this.events.sort((b: { date1: string | number | Date; }, a: { date1: string | number | Date; }) => new Date(b.date1).getTime() - new Date(a.date1).getTime());
       // this.places.sort((a: { age: number; }, b: { age: number; }) => (a.age < b.age) ? 1 : (a.age === b.age) ? ((a.age < b.age) ? 1 : -1) : -1 );
       console.log(this.events)
     })

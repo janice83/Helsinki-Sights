74416d49fd8e122f0d8dd5b5e58b829ab986d55b
janice83 <janna.lofman@gmail.com>
2022-04-21 10:55:11 +0300

    add content to event popup

--- hkiapp/src/app/popup.service.ts
+++ hkiapp/src/app/popup.service.ts
@@ -22,9 +22,15 @@ export class PopupService {
 
   // creates popup for event on map 
   makeEventPopup(data: any): string {
+    if(data.info_url == null || data.info_url == undefined) {
+      data.info_url = "";
+    }
+    const aika = new Date(data.event_dates.starting_day).toLocaleString('en-GB');
     return `` +
     `<div>Nimi: ${data.name.fi}
-    <p>Aika: ${data.event_dates.starting_day}</p>
+    <a class="mb-0 opacity-75 link-info" target="_blank" href="{{ data.info_url }}" onclick="window.open(this.href,'_blank');
+    "rel="noopener noreferrer">${ data.info_url }</a>
+    <p>Aika: ${aika}</p>
     <p>Osoite: ${data.location.address.street_address}, ${data.location.address.postal_code} ${data.location.address.locality}
     </div>`
     // `<div><a routerLink="${'/places-detail/' + data.id }>Nimi: ${data.name.fi}</a></div>`

d72686708e0fc8d73ffea507087c54e158716370
Josephine <josephine.closan@yahoo.com>
2022-04-26 13:53:32 +0300

    Create spring boot repo. Create controller to retrieve data from Open Api.

---
+++ hkiapp-backend/src/main/java/com/example/hkiappbackend/HkiController.java
@@ -0,0 +1,27 @@
+package com.example.hkiappbackend;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class HkiController {
+
+    @RequestMapping("/")
+    public String hello() {
+      return "Hello, Helsinki";
+    }
+  
+
+    @GetMapping("/v1/places")
+    private String getAllPlaces(){
+        String url = "https://open-api.myhelsinki.fi/v1/places/";
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(url, String.class);
+        return result;
+    }
+    
+}

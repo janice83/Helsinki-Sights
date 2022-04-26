896ada51006c8ba80be9a5f028e084e9b8f34569
janice83 <janna.lofman@gmail.com>
2022-04-26 14:47:18 +0300

    add function to calculate distance

--- hkiapp/src/app/places.service.ts
+++ hkiapp/src/app/places.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import { PlacesListComponent } from './places-list/places-list.component';
 import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs/internal/Observable';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlacesService {
+  currentLat: any;
+  currentLon: any;
 
   constructor(private http: HttpClient) { }
 
@@ -13,6 +16,9 @@ export class PlacesService {
     const places = this.http.get('assets/data/places.json');
     return places;
   }
+
 }
+
+
 
 
